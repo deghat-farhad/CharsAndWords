c9752a844823ae63e0fc96a2796b04220da90c41
Farhad_Deghat <deghat.farhad@gmail.com>
2020-02-25 12:03:55 +0330

    initial commit

---
+++ app/src/main/java/com/farhad/deghat/charsandwords/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.farhad.deghat.charsandwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
